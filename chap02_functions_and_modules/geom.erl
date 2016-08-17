f62296f36eaa2ae1fff412acd7d4f1f6bdb2e5c6
Vishal Lama <vishal.lama@gmail.com>
2016-08-17 08:37:05 -0600

    Document area function

--- chap02_functions_and_modules/geom.erl
+++ chap02_functions_and_modules/geom.erl
@@ -6,5 +6,10 @@
 -module(geom).
 -export([area/2]).
 
+%% @doc Calculates the area of a rectangle, given its length and width.
+%% Returns the product of length and width.
+
+-spec(area(number(), number()) -> number()).
+
 area(Length, Width) -> Length * Width.
 
