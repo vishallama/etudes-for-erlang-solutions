ad14e5d92d51524647a1733324c0de390ee51d06
Vishal Lama <vishal.lama@gmail.com>
2016-08-18 17:13:04 -0600

    Do Étude 4-5: Recursion with a Helper Function

--- chap04_logic_and_recursion/etude_4-3_non_tail_recursive_functions/powers.erl
+++ chap04_logic_and_recursion/etude_4-3_non_tail_recursive_functions/powers.erl
@@ -5,7 +5,7 @@
 %% @version 0.1
 
 -module(powers).
--export([raise/2]).
+-export([raise/2, nth_root/2]).
 
 %% @doc Raise a number X to an integer power N.
 %% Any number to the power of 0 equals 1.
@@ -21,4 +21,30 @@ raise(X, N) when N > 0 -> raise(X, N, 1).
 
 raise(_, 0, Accumulator) -> Accumulator;
 raise(X, N, Accumulator) -> raise(X, N-1, X * Accumulator).
+
+
+%% @doc Find the nth root of a given number.
+
+-spec(nth_root(number(), integer()) -> number()).
+
+nth_root(X, N) -> nth_root(X, N, X / 2.0).
+
+%% @doc Helper function to find an nth_root by passing an approximation
+%% from one call to the next.
+%% If the difference between current and next approximation is less than
+%% 1.0e-8, return the next approximation; otherwise, return
+%% nth_root(X, N, NextApproximation).
+
+nth_root(X, N, A) ->
+  io:format("Current guess is: ~w ~n", [A]),
+
+  F = raise(A, N) - X,
+  Fprime = N * raise(A, N-1),
+  Next = A - F / Fprime,
+  Change = abs(Next - A),
+
+  if
+    Change < 1.0e-8 -> Next;
+    true -> nth_root(X, N, Next)
+  end.
 
