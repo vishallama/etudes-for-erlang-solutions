0eabad093f922720c452287a5695e76dc9fdaba5
Vishal Lama <vishal.lama@gmail.com>
2016-08-20 10:42:34 -0600

    Add stats:range/1 and fix typo

--- chap06_lists/etude_6-1_recursive_iteration_through_a_list/stats.erl
+++ chap06_lists/etude_6-1_recursive_iteration_through_a_list/stats.erl
@@ -4,21 +4,21 @@
 %% @version 0.1
 
 -module(stats).
--export([mininum/1, maximum/1]).
+-export([minimum/1, maximum/1, range/1]).
 
-%% @doc Returns the mininum item in a list of numbers. Fails when given an
+%% @doc Returns the minimum item in a list of numbers. Fails when given an
 %% empty list.
 
--spec(mininum(list(number())) -> number()).
+-spec(minimum(list(number())) -> number()).
 
-mininum(List) -> mininum(List, hd(List)).
+minimum(List) -> minimum(List, hd(List)).
 
-mininum([], Current) ->
+minimum([], Current) ->
   Current;
-mininum([H|T], Current) when H < Current ->
-  mininum(T, H);
-mininum([_|T], Current) ->
-  mininum(T, Current).
+minimum([H|T], Current) when H < Current ->
+  minimum(T, H);
+minimum([_|T], Current) ->
+  minimum(T, Current).
 
 %% @doc Returns the maximum item in a list of numbers. Fails when given an
 %% empty list.
@@ -33,4 +33,14 @@ maximum([H|T], Current) when H > Current ->
   maximum(T, H);
 maximum([_|T], Current) ->
   maximum(T, Current).
+
+%% @doc Returns the mininum and maximum items in a list of numbers in the
+%% form [mininum, maximum]. Fails when given an empty list.
+
+-spec(range(list(number())) -> list(number())).
+
+range(List) ->
+  Minimum = minimum(List),
+  Maximum = maximum(List),
+  [Minimum, Maximum].
 
